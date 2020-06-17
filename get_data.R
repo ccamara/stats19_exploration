7026540a1e1a70da3fd1b42de45395e8aaab29fa
Carlos Cámara <ccamara@pm.me>
2020-06-17 21:26:14 +0100

    Improves report.

--- get_data.R
+++ get_data.R
@@ -3,18 +3,7 @@ source("init.R")
 
 # Data download -----------------------------------------------------------
 
-# years <- c(1979:2019)
-# 
-# for (i in years) {
-#   dl_stats19(year = i, data_dir = "data/raw/")
-# }
-
-dl_stats19(year = 2018, type = "Accidents", data_dir = "data/raw/", file_name = "2018_accidents.csv", silent = TRUE)
-dl_stats19(year = 2018)
-
-dl_stats19(year = 2018, type = "vehicles", data_dir = "data/raw/", file_name = "2018_vehicles.csv", silent = TRUE)
-
-years <- c(2016:2018)
+years <- c(2009:2018)
 
 for (i in years) {
   df <- get_stats19(year = i, type = "accident", ask = FALSE)
@@ -23,19 +12,10 @@ for (i in years) {
 }
 
 
-vehicles2018 <- get_stats19(year = 2018, type = "vehicles", ask = FALSE)
-accidents2018 <- get_stats19(year = 2018, type = "accident", ask = FALSE)
-accidents2017 <- get_stats19(year = 2017, type = "accident", ask = FALSE)
-accidents2016 <- get_stats19(year = 2016, type = "accident", ask = FALSE)
-accidents2015 <- get_stats19(year = 2015, type = "accident", ask = FALSE) %>% 
-  group_by(accident_severity) %>% 
-  tally() %>% 
-  rename(total_accidents = n)
-
-years <- c(2004:2018)
-
+# Calculate number of accidents per year and type. ------------------------
 
 datalist = list()
+
 for (i in years ) {
 
   tmp_summary_accidents <- get_stats19(year = i, type = "accident", ask = FALSE) %>% 
@@ -51,44 +31,34 @@ for (i in years ) {
   datalist[[i]] <- tmp_summary_accidents # add it to your list
 }
 
-summary_accidents <- dplyr::bind_rows(datalist)
+summary_accidents2 <- dplyr::bind_rows(datalist)
 
-accidents %>% 
-group_by(accident_severity) %>% 
-  tally() 
+write.csv(summary_accidents, file = "data/interim/summary_accidents.csv")
 
-accidents <- c(accidents2018, accidents2017, accidents2016, accidents2015)
 
-for (i in accidents) {
+# Calculate number of casualities per year --------------------------------
 
-  tmp_df <- as_tibble(i) %>% 
-    select(accident_severity)
-  # %>%
-  #   rename(severity = accident_severity) %>% 
-  #   group_by(severity) %>%
-  #   summarise(Total = n()) %>%
-  #   mutate(freq = Total/sum(Total)*100) %>%
-  #   # arrange(desc(Total)) %>% 
-  #   mutate(year = str_sub(deparse(substitute(i)), -4))
-  # 
+# Create an empty dataframe.
+summary_casualties = data.frame(
+  n = as.numeric(),
+  year = as.character()
+)
+
+for (i in years ) {
+  
+  tmp_summary_casualties <- get_stats19(year = i, type = "accident", ask = FALSE) %>% 
+    select(number_of_casualties) %>% 
+    tally() %>% 
+    mutate(year = as.character(i))
+
+  summary_casualties <- union(summary_casualties, tmp_summary_casualties) # add it to your list
 }
 
-summary_severity_accidents2018 <- accidents2018 %>% 
-  select(accident_severity) %>%
-  group_by(accident_severity) %>%
-  summarise(Total = n()) %>%
-  mutate(freq = Total/sum(Total)*100) %>%
-  # arrange(desc(Total)) %>% 
-  mutate(year = 2018)
-
-accidents2018 %>% 
-  group_by(accident_severity) %>% 
-  tally() 
-
-accidents2018 %>% 
-  count(accident_severity) 
-
-tmp_df <- as.tibble(accidents2018) 
+write.csv(summary_casualties, file = "data/interim/summary_casualties.csv")
 
 
-mtcars %>% count(cyl)
+# Download casualties -----------------------------------------------------
+
+casualties2018 <- get_stats19(year = 2018, type = "casualties", ask = FALSE) 
+
+write.csv(casualties2018, file = "data/raw/casualties2018.csv")
